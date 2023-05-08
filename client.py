ca3e49ecc864ba687fba1465475e6bdb234af95a
VIRTUALGOD325 <65289865+VIRTUALGOD325@users.noreply.github.com>
2023-05-08 23:44:52 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -61,15 +61,32 @@ def placeNumbers():
     global ticketGrid
     global currentNumberList
 
-    for row in range(0,3):
-        randomColList = []
-        counter = 0
+    numberContainer = {
+        "0": [1,2,3,4,5,6,7,8,9],
+        "1": [11,12,13,14,15,16,17,18,19],
+        "2": [21,22,23,24,25,26,27,28,29],
+        "3": [31,32,33,34,35,36,37,38,39],
+        "4": [41,42,43,44,45,46,47,48,49],
+        "5": [51,52,53,54,55,56,57,58,59],
+        "6": [61,62,63,64,65,66,67,68,69],
+        "7": [71,72,73,74,75,76,77,78,79],
+        "8": [81,82,83,84,85,86,87,88,89,90],
+    }
 
-        while counter<=4:
-            randomCol = random.randint(0,8)
-            if(randomCol not in randomColList):
-                randomColList.append(randomCol)
-                counter+=1
+    
+    counter = 0
+    randomColList = []
+    while(counter < len(randomColList)):
+        colNum = randomColList[counter]
+        numberListByIndex = numberContainer[str(colNum)]
+        randomNumber = random.choice(numberListByIndex) 
+    
+        if(randomNumber not in currentNumberList):
+            numberBox = ticketGrid[row][colNum]
+            numberBox.configure(text=randomNumber, fg="black")
+            currentNumberList.append(randomNumber)
+        
+            counter+=1
 
 
 
